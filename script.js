56b6c28ad29a4524ba1a9222e144eb003ff83bb8
qzqc <74603939+qzqc@users.noreply.github.com>
2024-06-04 22:17:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     const image = document.getElementById('display-image');
-    image.src = 'assets/IMG_4709.gif';
+    image.src = 'assets/IMG_4226.jpeg';
 });
