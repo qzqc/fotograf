2d6d83fd46cb6a2660ca663a34b7a55136e0be66
qzqc <74603939+qzqc@users.noreply.github.com>
2024-06-05 13:07:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const fileList = document.getElementById('file-list');
     const repo = 'qzqc/fotograf'; // GitHub kullanıcı adı ve depo adı
-    const path = 'assets';
+    const path = 'assets'; // Dosyaların bulunduğu klasör
 
     fetch(`https://api.github.com/repos/${repo}/contents/${path}`)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('GitHub API isteği başarısız oldu');
+            }
+            return response.json();
+        })
         .then(data => {
-            data.forEach(file => {
-                if (file.type === 'file') {
-                    const listItem = document.createElement('li');
-                    const link = document.createElement('a');
-                    link.href = file.download_url;
-                    link.textContent = file.name;
-                    listItem.appendChild(link);
-                    fileList.appendChild(listItem);
-                }
-            });
+            if (Array.isArray(data)) {
+                data.forEach(file => {
+                    if (file.type === 'file') {
+                        const listItem = document.createElement('li');
+                        const link = document.createElement('a');
+                        link.href = file.download_url;
+                        link.textContent = file.name;
+                        listItem.appendChild(link);
+                        fileList.appendChild(listItem);
+                    }
+                });
+            } else {
+                console.error('Beklenmeyen API yanıtı', data);
+            }
         })
         .catch(error => console.error('Hata:', error));
 });
